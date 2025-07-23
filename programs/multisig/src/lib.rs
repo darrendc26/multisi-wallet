f391671536b9554c464772b025b19dfcd4708b8a
Darren <darren26feb@gmail.com>
2025-07-23 08:48:55 +0530

    Structure

---
+++ programs/multisig/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("GLtnCFBPrFgisKinYNw5sW9s3VHM5J8KvtBncQfUWjY6");
+
+#[program]
+pub mod multisig {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
